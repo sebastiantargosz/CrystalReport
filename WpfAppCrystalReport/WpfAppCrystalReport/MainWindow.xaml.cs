8cc6bb99d950808a7fd9c48acffccc123adffaa0
sebastiantargosz <47144322+sebastiantargosz@users.noreply.github.com>
2020-01-08 17:53:21 +0100

    Add files via upload

---
+++ WpfAppCrystalReport/WpfAppCrystalReport/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppCrystalReport
+{
+	/// <summary>
+	/// Logika interakcji dla klasy MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			ReportDocument reportDocument = new ReportDocument();
+			string path = AppDomain.CurrentDomain.BaseDirectory + "CrystalReport1.rpt";
+
+			reportDocument.Load(path);
+			reportDocument.DataSourceConnections[0].IntegratedSecurity = true;
+			reportDocument.Refresh();
+
+			reportDocument.SetParameterValue("Id", 1);
+			viewer.ViewerCore.ReportSource = reportDocument;
+		}
+	}
+}
